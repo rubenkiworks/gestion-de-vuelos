b7c6d81c47b6bc0201f350050934bdde8f77a80b
rubenkiworks <rubengomezkiworks@gmail.com>
2024-10-15 15:56:53 +0200

    Ejercicio 2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -14,8 +15,9 @@ public class App
     {
         //Ejercicio 1
         List<Vuelo> vuelos = Vuelo.getVuelos();
-        vuelos.stream().filter(v -> v.getPasajeros().stream().count() == 3).forEach(System.out::println);
+        //vuelos.stream().filter(v -> v.getPasajeros().stream().count() == 3).forEach(System.out::println);
 
-        
+        //Ejercicio 2
+        vuelos.stream().filter(v -> v.getFechaSalida().equals(LocalDate.now())).forEach(System.out::println);
     }
 }
