c50f112f99b58f63ad23032da4c0fc4efe9dc39f
rubenkiworks <rubengomezkiworks@gmail.com>
2024-10-15 20:06:43 +0200

    ejercicio 8

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -12,43 +12,63 @@ import java.util.stream.Collectors;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main(String[] args) {
         //Ejercicio 1
         List<Vuelo> vuelos = Vuelo.getVuelos();
         vuelos.stream().filter(v -> v.getPasajeros().stream().count() == v.getNumeroPlazas())
-        .collect(Collectors.toList());
+                .collect(Collectors.toList());
 
         //Ejercicio 2
         vuelos.stream().filter(v -> v.getFechaSalida().equals(LocalDate.now()))
-        .collect(Collectors.toList());
+                .collect(Collectors.toList());
 
         //Ejercicio 3
         vuelos.stream().filter(v -> v.getDuracionVuelo() > 10)
-        .collect(Collectors.toList());
+                .collect(Collectors.toList());
 
         //Ejercicio 4
         vuelos.stream().filter(v -> v.getDuracionVuelo() > 24)
-        .collect(Collectors.toList());
+                .collect(Collectors.toList());
 
         //Ejercicio 5
-        vuelos.stream().max(Comparator.comparing(v -> v)).ifPresent(maxInt->System.out.println(maxInt));
-        
+        vuelos.stream().max(Comparator.comparing(v -> v)).ifPresent(maxInt -> System.out.println(maxInt));
+
         //Ejercicio 6
         Map<String, List<Vuelo>> pasajerosPorDestino = vuelos.stream()
                 .collect(Collectors.groupingBy(vuelo -> vuelo.getDestino()));
-        
+
         pasajerosPorDestino.entrySet().stream().forEach(entry -> {
-                    System.out.println("Para el destino " + entry.getKey());
-        
-                    entry.getValue().stream().
+            System.out.println("Para el destino " + entry.getKey());
+
+            entry.getValue().stream().
                     forEach(entry2 -> entry2.getPasajeros().stream().forEach(System.out::println));
-                });
-        
+        });
+
         // Ejercicio 7
-        vuelos.stream().filter(v -> v.getFechaSalida().getMonth().equals(v.getFechaSalida().plusDays(10).getMonth()))
-        .collect(Collectors.toList());
+        vuelos.stream().filter(v -> v.getFechaSalida().getMonth().
+                equals(v.getFechaSalida().plusDays(10).getMonth()))
+                .collect(Collectors.toList());
+
+        //Ejercicio 8
+        System.out.println("ejeercicio 8");
+        List<Pasajero> pasajeros = Pasajero.getPasajeros();
+        var pasajerosPorEdadSexo = pasajeros.stream()
+                .collect(Collectors.groupingBy(p -> p.getSexo(), Collectors.groupingBy(Pasajero::edad)));
+        
+            pasajerosPorEdadSexo.entrySet().stream().forEach(entry1 -> {
+            System.out.println("Por sexo: " + entry1.getKey());
+            Map<Long, List<Pasajero>> entry2 = entry1.getValue();
+
+            entry2.entrySet().stream().forEach(entry -> {
+                System.out.println("Por edad: " + entry.getKey());
+
+                entry.getValue().stream().forEach(System.out::println);
+
+            });
+        });
+
+        
     }
 }
--- src/main/java/com/example/Pasajero.java
+++ src/main/java/com/example/Pasajero.java
@@ -1,6 +1,10 @@
 package com.example;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,4 +22,113 @@ public class Pasajero {
     private String segundoApellido;
     private LocalDate fechaNacimiento;
     private Sexo sexo;
+
+    public Long edad(){
+        return ChronoUnit.YEARS.between(fechaNacimiento, LocalDate.now());
+    }
+
+    public static List<Pasajero> getPasajeros()  {
+
+        List<Pasajero> pasajeros = new ArrayList<>();
+
+        pasajeros.add(
+            Pasajero.builder()
+                .nombre("Ruben")
+                .primerApellido("Gomez")
+                .segundoApellido("Sabio")
+                .sexo(Sexo.HOMBRE)
+                .fechaNacimiento(LocalDate.of(2000, Month.SEPTEMBER,23))
+                .build()
+        );
+
+        pasajeros.add(
+            Pasajero.builder()
+                .nombre("Juan")
+                .primerApellido("Guerrero")
+                .segundoApellido("Leon")
+                .sexo(Sexo.HOMBRE)
+                .fechaNacimiento(LocalDate.of(1997, Month.MAY,23))
+                .build()
+        );
+
+        pasajeros.add(
+            Pasajero.builder()
+                .nombre("Ivan")
+                .primerApellido("Reina")
+                .segundoApellido("Galvez")
+                .sexo(Sexo.HOMBRE)
+                .fechaNacimiento(LocalDate.of(2001, Month.OCTOBER, 10))
+                .build()
+        );
+
+        pasajeros.add(
+            Pasajero.builder()
+                .nombre("Antonio")
+                .primerApellido("Saez")
+                .segundoApellido("Vagace")
+                .sexo(Sexo.HOMBRE)
+                .fechaNacimiento(LocalDate.of(1990, Month.DECEMBER,3))
+                .build()
+        );
+
+        pasajeros.add(
+            Pasajero.builder()
+                .nombre("Marcos")
+                .primerApellido("Perez")
+                .segundoApellido("Serrano")
+                .sexo(Sexo.HOMBRE)
+                .fechaNacimiento(LocalDate.of(1996, Month.APRIL, 23))
+                .build()
+        );
+        pasajeros.add(
+            Pasajero.builder()
+                .nombre("Elli")
+                .primerApellido("Fdez")
+                .segundoApellido("Hdez")
+                .sexo(Sexo.MUJER)
+                .fechaNacimiento(LocalDate.of(2005, Month.SEPTEMBER,14))
+                .build()
+        );
+
+        pasajeros.add(
+            Pasajero.builder()
+                .nombre("Paca")
+                .primerApellido("Fdez")
+                .segundoApellido("Hdez")
+                .sexo(Sexo.MUJER)
+                .fechaNacimiento(LocalDate.of(2004, Month.JULY,14))
+                .build()
+        );
+        pasajeros.add(
+            Pasajero.builder()
+                .nombre("Elli")
+                .primerApellido("Fdez")
+                .segundoApellido("Mtnez")
+                .sexo(Sexo.MUJER)
+                .fechaNacimiento(LocalDate.of(2005, Month.SEPTEMBER,14))
+                .build()
+        );
+
+        pasajeros.add(
+            Pasajero.builder()
+                .nombre("Paca")
+                .primerApellido("Mtnez")
+                .segundoApellido("Hdez")
+                .sexo(Sexo.MUJER)
+                .fechaNacimiento(LocalDate.of(2004, Month.JULY,14))
+                .build()
+        );
+
+        pasajeros.add(
+            Pasajero.builder()
+                .nombre("Marcos")
+                .primerApellido("Serrano")
+                .segundoApellido("Serrano")
+                .sexo(Sexo.HOMBRE)
+                .fechaNacimiento(LocalDate.of(1996, Month.APRIL, 23))
+                .build()
+        );
+
+        return pasajeros;
+    }
 }
